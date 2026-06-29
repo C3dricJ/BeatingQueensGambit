d1977e3d6a5d3197fb176a4883f0af2519210f4f
C3dricJ <cjurbino@gmail.com>
2026-06-29 10:28:22 -0500

    Add board helper methods for moves possibilities

--- BeatingQueensGambit.Core/Board/Chessboard.cs
+++ BeatingQueensGambit.Core/Board/Chessboard.cs
@@ -1,4 +1,6 @@
 using BeatingQueensGambit.Core.Pieces;
+using BeatingQueensGambit.Core.Models;
+using BeatingQueensGambit.Core.Enums;
 
 namespace BeatingQueensGambit.Core.Board;
 
@@ -8,4 +10,55 @@ namespace BeatingQueensGambit.Core.Board;
 public class ChessBoard
 {
     public Piece?[,] Squares { get; } = new Piece?[8,8];
+    public Piece? GetPiece(Position position)
+    {
+        return Squares[position.Row, position.Column];
+    }
+
+    public void SetPiece(Position position, Piece? piece)
+    {
+        Squares[position.Row, position.Column] = piece;
+    }
+
+    public bool IsInsideBoard(Position position)
+    {
+        return
+        position.Row >= 0 &&
+        position.Row < 8 &&
+        position.Column >= 0 &&
+        position.Column < 8;
+    }
+
+    public bool IsEmpty(Position position)
+    {
+        return
+        IsInsideBoard(position) &&
+        GetPiece(position) == null;
+    }
+
+    public bool HasEnemyPiece(
+    Position position,
+    PieceColor myColor)
+    {
+    if (!IsInsideBoard(position))
+        return false;
+
+    var piece = GetPiece(position);
+
+    return piece != null &&
+           piece.Color != myColor;
+    }
+
+    public bool HasFriendlyPiece(
+    Position position,
+    PieceColor myColor)
+{
+    if (!IsInsideBoard(position))
+        return false;
+
+    var piece = GetPiece(position);
+
+    return piece != null &&
+           piece.Color == myColor;
+    }
 }
