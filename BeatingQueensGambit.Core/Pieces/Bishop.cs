19e39c9e7d233b1390b5a686deeb0709ca58b229
C3dricJ <cjurbino@gmail.com>
2026-06-29 15:29:10 -0500

    Implement bishop movement

--- BeatingQueensGambit.Core/Pieces/Bishop.cs
+++ BeatingQueensGambit.Core/Pieces/Bishop.cs
@@ -1,6 +1,7 @@
 using BeatingQueensGambit.Core.Enums;
 using BeatingQueensGambit.Core.Models;
 using BeatingQueensGambit.Core.Board;
+using System.Collections.Generic;
 
 namespace BeatingQueensGambit.Core.Pieces;
 
@@ -14,7 +15,51 @@ public class Bishop : Piece
     public override IEnumerable<Position> GetLegalMoves(
         ChessBoard board,
         Position currentPosition)
+    {
+        var legalMoves = new List<Position>();
+
+        int[,] directions =
         {
-           return Enumerable.Empty<Position>();
+            {-1,-1},
+            {-1, 1},
+            { 1,-1},
+            { 1, 1}
+        };
+
+        for (int i = 0; i < directions.GetLength(0); i++)
+        {
+            int rowDirection = directions[i, 0];
+            int columnDirection = directions[i, 1];
+
+            int row = currentPosition.Row + rowDirection;
+            int column = currentPosition.Column + columnDirection;
+
+            while (true)
+            {
+                var position = new Position(row, column);
+
+                if (!board.IsInsideBoard(position))
+                    break;
+
+                if (board.IsEmpty(position))
+                {
+                    legalMoves.Add(position);
+                }
+                else
+                {
+                    if (board.HasEnemyPiece(position, Color))
+                    {
+                        legalMoves.Add(position);
+                    }
+
+                    break;
+                }
+
+                row += rowDirection;
+                column += columnDirection;
+            }
         }
+
+        return legalMoves;
+    }
 }
