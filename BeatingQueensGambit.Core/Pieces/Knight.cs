69a77157cdd343cee49115071ae6fa3c413a83e8
C3dricJ <cjurbino@gmail.com>
2026-06-29 15:26:06 -0500

    Implement knight movement

--- BeatingQueensGambit.Core/Pieces/Knight.cs
+++ BeatingQueensGambit.Core/Pieces/Knight.cs
@@ -1,6 +1,7 @@
 using BeatingQueensGambit.Core.Enums;
 using BeatingQueensGambit.Core.Models;
 using BeatingQueensGambit.Core.Board;
+using System.Collections.Generic;
 
 namespace BeatingQueensGambit.Core.Pieces;
 
@@ -10,11 +11,41 @@ public class Knight : Piece
         : base(color, PieceType.Knight)
     {
     }
- 
+
     public override IEnumerable<Position> GetLegalMoves(
         ChessBoard board,
         Position currentPosition)
+    {
+        var legalMoves = new List<Position>();
+
+        int[,] offsets =
         {
-           return Enumerable.Empty<Position>();
+            {-2,-1},
+            {-2, 1},
+            {-1,-2},
+            {-1, 2},
+            { 1,-2},
+            { 1, 2},
+            { 2,-1},
+            { 2, 1}
+        };
+
+        for (int i = 0; i < offsets.GetLength(0); i++)
+        {
+            var move = new Position(
+                currentPosition.Row + offsets[i,0],
+                currentPosition.Column + offsets[i,1]);
+
+            if (!board.IsInsideBoard(move))
+                continue;
+
+            if (board.IsEmpty(move) ||
+                board.HasEnemyPiece(move, Color))
+            {
+                legalMoves.Add(move);
+            }
         }
+
+        return legalMoves;
+    }
 }
