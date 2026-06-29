fb0fe06c7d16d39490c7a9766b5e8d283f7e5465
C3dricJ <cjurbino@gmail.com>
2026-06-29 15:36:23 -0500

    Implemented king movement

--- BeatingQueensGambit.Core/Pieces/King.cs
+++ BeatingQueensGambit.Core/Pieces/King.cs
@@ -1,6 +1,7 @@
 using BeatingQueensGambit.Core.Enums;
 using BeatingQueensGambit.Core.Models;
 using BeatingQueensGambit.Core.Board;
+using System.Collections.Generic;
 
 namespace BeatingQueensGambit.Core.Pieces;
 
@@ -11,11 +12,43 @@ public class King : Piece
     {
     }
 
-    
     public override IEnumerable<Position> GetLegalMoves(
         ChessBoard board,
         Position currentPosition)
+    {
+        var legalMoves = new List<Position>();
+
+        int[][] directions =
         {
-           return Enumerable.Empty<Position>();
+            new[] {-1,-1},
+            new[] {-1, 0},
+            new[] {-1, 1},
+            new[] { 0,-1},
+            new[] { 0, 1},
+            new[] { 1,-1},
+            new[] { 1, 0},
+            new[] { 1, 1}
+        };
+
+        foreach (var direction in directions)
+        {
+            var nextPosition = new Position(
+                currentPosition.Row + direction[0],
+                currentPosition.Column + direction[1]);
+
+            if (!board.IsInsideBoard(nextPosition))
+                continue;
+
+            if (board.IsEmpty(nextPosition))
+            {
+                legalMoves.Add(nextPosition);
+            }
+            else if (board.HasEnemyPiece(nextPosition, Color))
+            {
+                legalMoves.Add(nextPosition);
+            }
         }
+
+        return legalMoves;
+    }
 }
