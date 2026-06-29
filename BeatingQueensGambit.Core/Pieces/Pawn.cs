a49b764fe5f1f9d5bba7cd661652a69368845ccc
C3dricJ <cjurbino@gmail.com>
2026-06-29 15:13:56 -0500

    Implement basic pawn forward movement

--- BeatingQueensGambit.Core/Pieces/Pawn.cs
+++ BeatingQueensGambit.Core/Pieces/Pawn.cs
@@ -1,6 +1,7 @@
 using BeatingQueensGambit.Core.Enums;
 using BeatingQueensGambit.Core.Models;
 using BeatingQueensGambit.Core.Board;
+using System.Collections.Generic;
 
 namespace BeatingQueensGambit.Core.Pieces;
 
@@ -12,9 +13,22 @@ public class Pawn : Piece
     }
 
     public override IEnumerable<Position> GetLegalMoves(
-        ChessBoard board,
-        Position currentPosition)
+    ChessBoard board,
+    Position currentPosition)
     {
-        return Enumerable.Empty<Position>();
+        var legalMoves = new List<Position>();
+
+        int direction = IsWhite ? -1 : 1;
+
+        var oneForward = new Position(
+            currentPosition.Row + direction,
+            currentPosition.Column);
+
+        if (board.IsEmpty(oneForward))
+        {
+            legalMoves.Add(oneForward);
+        }
+
+        return legalMoves;
     }
 }
